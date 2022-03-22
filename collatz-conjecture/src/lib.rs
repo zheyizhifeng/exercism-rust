fc95aebb0e90ec0c3d9e5693c89ee5d46d91d9b9
lucida <zheyizhifeng@126.com>
2022-03-22 09:50:53 +0800

    :white_check_mark: update collatz-conjecture

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,6 +1,24 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    match n {
+        0 => None,
+        1 => Some(0),
+        n => {
+            let mut step = 0_u64;
+            let mut n = n;
+            while n > 1 {
+                if (n & 1) == 1 {
+                    let m = n.checked_mul(3);
+                    if m.is_none() || m?.checked_add(1).is_none() {
+                        return None;
+                    }
+                    n = m.unwrap() + 1;
+                } else {
+                    n /= 2;
+                }
+                step += 1;
+            }
+            Some(step)
+        }
+    }
+
 }
